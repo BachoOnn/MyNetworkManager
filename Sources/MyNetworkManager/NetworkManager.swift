c6dd4953ed1f847296e9ff2f1f1c553b5b3fdd28
BachoOnn <oniani.bachoo@gmail.com>
2026-01-17 19:34:04 +0400

    update

--- Sources/MyNetworkManager/NetworkManager.swift
+++ Sources/MyNetworkManager/NetworkManager.swift
@@ -22,76 +22,70 @@ public final class NetworkManager {
         self.decoder = decoder
         self.encoder = encoder
     }
-        
-    /// Generic fetch for requests without body
+    
+    // MARK: - Public Fetch Methods
+    
+    /// Fetch WITHOUT body (GET, DELETE)
     public func fetch<T: Codable & Sendable>(
         urlString: String,
         method: HTTPMethodType = .get,
         headers: [String: String]? = nil
     ) async throws -> T {
-        try await performRequest(
-            urlString: urlString,
-            method: method,
-            body: nil as String?,
-            headers: headers
-        )
-    }
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
         
-    /// Generic fetch for requests with body
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        headers?.forEach { key, value in
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        let (data, response) = try await session.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.statusCode(httpResponse.statusCode)
+        }
+        
+        return try decoder.decode(T.self, from: data)
+    }
+    
+    /// Fetch WITH body (POST, PUT, PATCH)
     public func fetch<T: Codable & Sendable, U: Codable>(
         urlString: String,
         method: HTTPMethodType,
         body: U,
         headers: [String: String]? = nil
     ) async throws -> T {
-        try await performRequest(
-            urlString: urlString,
-            method: method,
-            body: body,
-            headers: headers
-        )
-    }
-        
-    private func performRequest<T: Codable & Sendable, U: Codable>(
-        urlString: String,
-        method: HTTPMethodType,
-        body: U?,
-        headers: [String: String]?
-    ) async throws -> T {
-        // Validate URL
         guard let url = URL(string: urlString) else {
             throw NetworkError.invalidURL
         }
         
-        // Create request
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         
-        // Add headers
         headers?.forEach { key, value in
             request.setValue(value, forHTTPHeaderField: key)
         }
         
-        // Add body if present
-        if let body = body {
-            request.httpBody = try encoder.encode(body)
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        }
+        request.httpBody = try encoder.encode(body)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        // Perform request
         let (data, response) = try await session.data(for: request)
         
-        // Validate response
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkError.invalidResponse
         }
         
-        // Check status code
         guard (200...299).contains(httpResponse.statusCode) else {
             throw NetworkError.statusCode(httpResponse.statusCode)
         }
         
-        // Decode and return
         return try decoder.decode(T.self, from: data)
     }
 }
